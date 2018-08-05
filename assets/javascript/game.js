ad7fac121dcb9603fa2576b00cdd3fa13bbe6887
JPlukarski <jwplukarski@gmail.com>
2018-08-05 14:40:46 -0500

    Finished CSS for the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,15 +10,25 @@ var wordList = [
     ["m", "a", "d", "i", "s", "o", "n"],
     ["c", "e", "r", "m", "a", "k"],
     ["v", "i", "n", "c", "e", "n", "n", "e", "s"]
-]
+];
 
-var gameScore = document.querySelector(".totalWins")
-gameScore = 2
+
+
+
+
+
+
+
+
+
 
 //When the start button is hit, the game will work. After it is hit once the text will change to "Reset Game" and subseqently hitting that will... well... restart the game
 function startFunction() {
 
-    //"Gets" the reset button from the HTML page:
+    var winTally = 0;
+    document.querySelector(".totalWins").textContent = winTally
+
+    //Turns the start button into the reset button on the HTML page:
     document.querySelector(".startButton").innerHTML = "Reset Game";
     //creates variable called "currentWord" and gives it a value of a random string from wordList:
     var currentWord = wordList[Math.floor(Math.random() * wordList.length)];
@@ -42,13 +52,15 @@ function startFunction() {
     //First, put each letter keyed into an array called "nextGuess"
     var nextGuess = [];
 
+    var remainingLetters = currentWord.length;
+    console.log(remainingLetters);
 
 
     //When a key is pressed: 
 
-    //creates the number of remaining guesses when the game is started
+    //creates the number of remaining guesses when the game i≈s started
 
-    var remainingGuesses = 12 - 1
+    var remainingGuesses = 12
 
     document.onkeyup = function (event) {
         //The pressed key is "captured" into this variable
@@ -60,8 +72,17 @@ function startFunction() {
 
         document.querySelector(".guessCount").innerHTML = remainingGuesses--
         if (remainingGuesses === -1) {
-            alert("You Suck!")
+            alert("Hey! Don't put ketchup on your dog!");
+            startFunction();
+
         }
+        if (remainingLetters === 0) {
+            alert("You Won!");
+            startFunction();
+            winTally++;
+        };
+
+
         for (index1 = 0; index1 < currentWord.length; index1++) {
 
             if (currentWord[index1] !== userGuess) {
@@ -74,8 +95,11 @@ function startFunction() {
                 console.log(wordAnswer);
                 targetDiv.innerHTML = wordAnswer.join(" ");
                 remainingGuesses++;
-                console.log(remainingGuesses)
+                remainingLetters--;
+                console.log(remainingLetters);
             };
+
+
         }
     }
 };
